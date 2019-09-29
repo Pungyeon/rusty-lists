a8e57cf5351c612daf1f25b166cac786ea6d5c19
Lasse Martin Jakobsen <lmj@corti.ai>
2019-09-29 13:34:02 +0200

    finished implementation of IterMut for Stack

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,27 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> Stack<T> {
+    fn iter(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut  T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.value
+        })
+    }
+}
+
 struct Stack<T> {
     head: Option<Box<Node<T>>>,
 }
