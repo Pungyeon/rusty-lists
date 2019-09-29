0190f1f0a1ac82bc39b5cd6a8d74a0d48ca90e3e
Lasse Martin Jakobsen <lmj@corti.ai>
2019-09-29 13:12:17 +0200

    Iter implemented for Stack

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,27 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> Stack<T> {
+    fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.value
+        })
+    }
+}
+
 struct Stack<T> {
     head: Option<Box<Node<T>>>,
 }
